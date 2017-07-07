a939826d8b797b35cdc33b0c16fc2341011322db
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-07-07 13:16:53 -0400

    added pasre.toString()

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
   
-  botResponse = cool() + ' @';
+  botResponse = cool() + parse.toString(5);
 
   options = {
     hostname: 'api.groupme.com',
