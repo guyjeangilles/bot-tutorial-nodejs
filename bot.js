8062d36fc0dbc8be561a3f706867b62c3dc3157e
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-07-07 12:51:35 -0400

    testing pashing variables

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool dude$/;
+      botRegex = /^\/$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(request.text);
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,10 +18,10 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(request.text) {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = request.text;
 
   options = {
     hostname: 'api.groupme.com',
