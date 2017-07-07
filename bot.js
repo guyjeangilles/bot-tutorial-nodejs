eea5f3ef4a0c36cb041b3eb2f38710991f7b9c2e
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-07-07 13:19:27 -0400

    text concat

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
   
-  botResponse = cool() + parse.toString(5);
+  botResponse = cool() + ' @Guyrandy Jean-Gilles';
 
   options = {
     hostname: 'api.groupme.com',
