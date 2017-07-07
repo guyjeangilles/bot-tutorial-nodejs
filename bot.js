5a18f9f67fb56595f8d2069e66e946888257823c
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-07-07 12:53:39 -0400

    add parse.toString

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/$/;
+      botRegex = /^\/cool guy$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage(request.text) {
   var botResponse, options, body, botReq;
 
-  botResponse = request.text;
+  botResponse = parse.toString(request.text);
 
   options = {
     hostname: 'api.groupme.com',
