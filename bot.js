a20bd5bf41c6b0c647d183a6827532b9b600f68a
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-07-08 17:08:48 -0400

    implemented different functions for different gender requests

--- bot.js
+++ bot.js
@@ -11,18 +11,18 @@ function respond() {
 
   if(request.text && botRegexGuy.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessageGuy();
     this.res.end();
   }
   if (request.text && botRegexGal.test(request.text)) {
 	this.res.writeHead(200);
-    postMessage();
+    postMessageGal();
     this.res.end();
   }
   
   if(request.text && botRegexNeutral.test(request.text)) {
 	this.res.writeHead(200);
-    postMessage();
+    postMessageNeutral();
     this.res.end();
   } else {
     console.log("don't care");
@@ -30,8 +30,6 @@ function respond() {
     this.res.end();
   }
 }
-
-function postMessage() {
   var neutral = ['Aaron McClendon', 'Abbie Spector', 'Abigail Schneider', 'Adam Rukin', 'Aleck Pinto', 'Alex Gaggino', 'Alexandra Bacchus', 'Amanda Halacy', 'Arjun Nukal', 'B O\'B', 'Beans', 'Becca Groner', 'Brandon Davenport', 'Briana Natalie', 'Bridget Lanigan', 'Brooke Sterneck', 'Catherine Levins', 'Corinne Sullivan', 'Courtney Morgan', 'Danya B', 'David Yocum', 'Dawn musil', 'Dayo Akinjisola', 'Drew Carlson', 'Eli Panken', 'Ella Simmons', 'Evan Matuszak', 'Gene Williams', 'Grettie Mason', 'Guyrandy Jean-Gilles', 'Ibanca Anand', 'Jaafar Mothafer', 'James Dolgin', 'James Levine', 'Jeannie blackwood', 'Jeff Biestek', 'Joe Sullivan', 'Jon Weiss', 'Jordan Symone', 'Josh Harrison', 'Joshua Su', 'Julia Wang', 'Justin Ramos', 'Katie Connors', 'Kelly Ready', 'Kelsey Murphy', 'Kimmi Schonhorst', 'Liv Sisson', 'Liv Stromme', 'Liz Treacy', 'Lydia Ottaviano', 'Mady Jankowski', 'Mallory Michaelis', 'Martha Cosgrove', 'Matt Bee', 'Matt Crescimanno', 'Micah Jaffe', 'Micah Leinbach', 'Michelle Khalid', 'Miguel Ramirez', 'Mihir Pershad', 'Nick Zajciw', 'Noah Zweben', 'Paola Peraza', 'Perrin Brown', 'Petey DeJoy', 'Rachel Smedley', 'Rahul Narain', 'Ryan Cleary', 'Sally Lindsay', 'Sam Einhorna', 'Sam Koening', 'Sam Summer', 'Sarena Martinez', 'Saseen Najjar', 'Sean McCroskey', 'Spencer Keith', 'Steven Soto', 'Thomas Krumins', 'Vinay Nagaraj', 'Zac Levin'];
   var gals = ['Abbie Spector', 'Abigail Schneider', 'Alexandra Bacchus', 'Amanda Halacy', 'Becca Groner', 'Briana Natalie', 'Bridget Lanigan', 'Brooke Sterneck', 'Catherine Levins', 'Corinne Sullivan', 'Courtney Morgan', 'Danya B', 'Dawn musil', 'Ella Simmons', 'Grettie Mason', 'Ibanca Anand', 'Jeannie blackwood', 'Jordan Symone', 'Julia Wang', 'Kate Connors', 'Kelly Ready', 'Kelsey Murphy', 'Kimmi Schonhorst', 'Liv Sisson', 'Liv Stromme', 'Liz Treacy', 'Lydia Ottaviano', 'Mady Jankowski', 'Mallory Michaelis', 'Martha Cosgrove', 'Michelle Khalid', 'Paola Peraza', 'Perrin Brown', 'Rachel Smedley', 'Sally Lindsay', 'Sarena Martinez'];
   var guys = [ 'Aaron McClendon', 'Adam Rukin', 'Aleck Pinto', 'Alex Gaggino', 'Arjun Nukal', 'B O\'B', 'Beans', 'Brandon Davenport', 'David Yocum', 'Dayo Akinjisola', 'Drew Carlson', 'Eli Panken', 'Evan Matuszak', 'Gene Wiliams', 'Guyrandy Jean-Gilles', 'Jaafar Mothafer', 'James Dolgin', 'James Levine', 'Jeff Biestek', 'Joe Sullivan', 'Jon Weiss', 'Josh Harrison', 'Joshua Su', 'Justin Ramos', 'Katie Connors', 'Matt Bee', 'Matt Crescinmanno', 'Micah Jaffe', 'Micah Leinbach', 'Miguel Ramirez', 'Mihir Pershad', 'Nick Zajciw', 'Noah Zweben', 'Petey DeJoy', 'Rahul Narain', 'Ryan Cleary', 'Sam Einhorna', 'Sam Koening', 'Sam Summer', 'Saseen Najjar', 'Sean McCroskey', 'Spencer Keith', 'Steven Soto', 'Thomas Krumins', 'Vinay Nagaraj', 'Zac Levin'];
@@ -40,6 +38,13 @@ function postMessage() {
   
   var neutralIndex = Math.floor(Math.random()*neutral.length);
   var match = neutral[neutralIndex];
+function postMessageGuy() {
+  var guys = [ 'Aaron McClendon', 'Adam Rukin', 'Aleck Pinto', 'Alex Gaggino', 'Arjun Nukal', 'B O\'B', 'Beans', 'Brandon Davenport', 'David Yocum', 'Dayo Akinjisola', 'Drew Carlson', 'Eli Panken', 'Evan Matuszak', 'Gene Wiliams', 'Guyrandy Jean-Gilles', 'Jaafar Mothafer', 'James Dolgin', 'James Levine', 'Jeff Biestek', 'Joe Sullivan', 'Jon Weiss', 'Josh Harrison', 'Joshua Su', 'Justin Ramos', 'Katie Connors', 'Matt Bee', 'Matt Crescinmanno', 'Micah Jaffe', 'Micah Leinbach', 'Miguel Ramirez', 'Mihir Pershad', 'Nick Zajciw', 'Noah Zweben', 'Petey DeJoy', 'Rahul Narain', 'Ryan Cleary', 'Sam Einhorna', 'Sam Koening', 'Sam Summer', 'Saseen Najjar', 'Sean McCroskey', 'Spencer Keith', 'Steven Soto', 'Thomas Krumins', 'Vinay Nagaraj', 'Zac Levin'];
+  
+  var botResponse, options, body, botReq;
+  
+  var index = Math.floor(Math.random()*guys.length);
+  var match = guys[index];
   
   //botResponse = cool() + ' @Guyrandy Jean-Gilles';
   botResponse ='^@' + match;
@@ -74,5 +79,86 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function postMessageGal() {
+  var gals = ['Abbie Spector', 'Abigail Schneider', 'Alexandra Bacchus', 'Amanda Halacy', 'Becca Groner', 'Briana Natalie', 'Bridget Lanigan', 'Brooke Sterneck', 'Catherine Levins', 'Corinne Sullivan', 'Courtney Morgan', 'Danya B', 'Dawn musil', 'Ella Simmons', 'Grettie Mason', 'Ibanca Anand', 'Jeannie blackwood', 'Jordan Symone', 'Julia Wang', 'Kate Connors', 'Kelly Ready', 'Kelsey Murphy', 'Kimmi Schonhorst', 'Liv Sisson', 'Liv Stromme', 'Liz Treacy', 'Lydia Ottaviano', 'Mady Jankowski', 'Mallory Michaelis', 'Martha Cosgrove', 'Michelle Khalid', 'Paola Peraza', 'Perrin Brown', 'Rachel Smedley', 'Sally Lindsay', 'Sarena Martinez'];
+  
+  var botResponse, options, body, botReq;
+  
+  var index = Math.floor(Math.random()*gals.length);
+  var match = gals[index];
+  
+  //botResponse = cool() + ' @Guyrandy Jean-Gilles';
+  botResponse ='^@' + match;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessageNeutral() {
+  var neutral = ['Aaron McClendon', 'Abbie Spector', 'Abigail Schneider', 'Adam Rukin', 'Aleck Pinto', 'Alex Gaggino', 'Alexandra Bacchus', 'Amanda Halacy', 'Arjun Nukal', 'B O\'B', 'Beans', 'Becca Groner', 'Brandon Davenport', 'Briana Natalie', 'Bridget Lanigan', 'Brooke Sterneck', 'Catherine Levins', 'Corinne Sullivan', 'Courtney Morgan', 'Danya B', 'David Yocum', 'Dawn musil', 'Dayo Akinjisola', 'Drew Carlson', 'Eli Panken', 'Ella Simmons', 'Evan Matuszak', 'Gene Williams', 'Grettie Mason', 'Guyrandy Jean-Gilles', 'Ibanca Anand', 'Jaafar Mothafer', 'James Dolgin', 'James Levine', 'Jeannie blackwood', 'Jeff Biestek', 'Joe Sullivan', 'Jon Weiss', 'Jordan Symone', 'Josh Harrison', 'Joshua Su', 'Julia Wang', 'Justin Ramos', 'Katie Connors', 'Kelly Ready', 'Kelsey Murphy', 'Kimmi Schonhorst', 'Liv Sisson', 'Liv Stromme', 'Liz Treacy', 'Lydia Ottaviano', 'Mady Jankowski', 'Mallory Michaelis', 'Martha Cosgrove', 'Matt Bee', 'Matt Crescimanno', 'Micah Jaffe', 'Micah Leinbach', 'Michelle Khalid', 'Miguel Ramirez', 'Mihir Pershad', 'Nick Zajciw', 'Noah Zweben', 'Paola Peraza', 'Perrin Brown', 'Petey DeJoy', 'Rachel Smedley', 'Rahul Narain', 'Ryan Cleary', 'Sally Lindsay', 'Sam Einhorna', 'Sam Koening', 'Sam Summer', 'Sarena Martinez', 'Saseen Najjar', 'Sean McCroskey', 'Spencer Keith', 'Steven Soto', 'Thomas Krumins', 'Vinay Nagaraj', 'Zac Levin'];
+  
+  var botResponse, options, body, botReq;
+  
+  var index = Math.floor(Math.random()*neutral.length);
+  var match = neutral[index];
+  
+  //botResponse = cool() + ' @Guyrandy Jean-Gilles';
+  botResponse ='^@' + match;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
